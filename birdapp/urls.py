b6a7ee34482d5ffe3749693a2f90b4f7f9fd2c7e
mkerekes1427 <mkerekes53@gmail.com>
2023-06-30 14:47:23 -0400

    Contact and email forms

--- birdapp/urls.py
+++ birdapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("contact/", views.contact, name="contact")
+    path("contact/", views.contact, name="contact"),
+    path("bird_email", views.bird_email, name="bird_email")
 ]
--- birdapp/views.py
+++ birdapp/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Home</h1>")
+    return render(request, "home.html")
+
+def bird_email(request):
+
+    if request.method == "POST":
+
+        data = {}
+
+        data["email"] = request.POST["email"]
+        data["bird_count"] = request.POST["bird_count"]
+        data["species"] = request.POST.getlist("species")
+        data["message"] = request.POST["message"]
+
+        return JsonResponse(data)
+
+    return render(request, "bird_email.html")
 
 def contact(request):
-    return HttpResponse("<h1>Contact</h1>")
+    return render(request, "contact.html")
