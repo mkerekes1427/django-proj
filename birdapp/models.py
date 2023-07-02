531354f8a9ccc1d301ec4dd8ca128091370ec9c7
mkerekes1427 <mkerekes53@gmail.com>
2023-07-01 21:09:28 -0400

    observation model and admin page

--- birdapp/models.py
+++ birdapp/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-# Person name
-# Date
-# Bird Name
-# Bird Category
-# Bird Picture
-# Description
+
+class Observation(models.Model):
+
+    person = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    observation_date = models.DateField(null=True, blank=True)
+    bird_name = models.CharField(null=False, blank=False, max_length=255)
+    bird_category = models.CharField(null=False, blank=False, max_length=255)
+
+    description = models.TextField(null=False, blank=False, max_length=1000)
+    color = models.CharField(null=False, blank=False, max_length=100)
+
+    def __str__(self):
+        return f'{self.person} {self.bird_name}'
+
+
+
+
